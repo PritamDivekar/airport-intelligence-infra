dcc469388784bd60553d27e5e689609817877713
PritamDivekar <114807288+PritamDivekar@users.noreply.github.com>
2026-01-30 12:19:20 +0530

    Delete terraform directory

--- terraform/glue/glue_database.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_glue_catalog_database" "airport_intelligence_db" {
-  name = var.glue_database_name
-}
--- terraform/glue/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "glue_role_arn" {
-  type = string
-}
-
-variable "glue_database_name" {
-  type = string
-}

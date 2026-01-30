9d9f1d378fe6e02ca984c2fc0807a58fbed0122e
PritamDivekar <114807288+PritamDivekar@users.noreply.github.com>
2026-01-30 12:19:35 +0530

    Delete etl directory

--- etl/etl_congestionScore.py
+++
@@ -1,6 +0,0 @@
-import sys
-from awsglue.utils import getResolvedOptions
-
-args = getResolvedOptions(sys.argv, [])
-
-print("Running Congestion Score ETL Job")
--- etl/etl_customers.py
+++
@@ -1,6 +0,0 @@
-import sys
-from awsglue.utils import getResolvedOptions
-
-args = getResolvedOptions(sys.argv, [])
-
-print("Running Customers ETL Job")
